97363134f0a26cdf3164ebc33404ab9896c74cf5
JaeseoKim <devjaeseo@gmail.com>
2020-08-05 18:24:04 +0900

    Go Routine Finsh!
    fast urlchecker!

--- main.go
+++ main.go
@@ -1,13 +1,46 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/JaeSeoKim/learngo/dict"
+	"net/http"
 )
 
+type res struct {
+	url    string
+	result string
+}
+
 func main() {
-	dictionary := dict.Dictionary{"first": "first", "second": "second", "third": "third"}
-	dictionary.Delete("h")
-	fmt.Println(dictionary.Search("h"))
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+
+	c := make(chan res)
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+	for i := 0; i < len(urls); i++ {
+		fmt.Println(<-c)
+	}
+}
+
+var errRequestsFailed = errors.New("Requests Failed")
+
+func hitURL(url string, c chan<- res) {
+	fmt.Println("Checking: ", url)
+	result := "OK"
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		result = "FAILED"
+	}
+	c <- res{url: url, result: result}
 }
